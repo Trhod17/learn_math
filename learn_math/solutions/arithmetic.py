fd3f0b393d3b041a55e81f97552bed70555cf0f6
Dax Mickelson <dmickels@cisco.com>
2019-12-10 09:17:38 -0700

    Fix typing

--- learn_math/solutions/arithmetic.py
+++ learn_math/solutions/arithmetic.py
@@ -1,13 +1,15 @@
 """Methods for computing arithmetic calculations"""
-from typing import List, SupportsFloat
+from typing import List, TypeVar
 
 addition_list = [1.1, 2, 3, -4, 5]
 subtraction_list = [100, 99, -123.123, 0]
 multiplication_list = [2, 3, 5.5, -1]
 division_list = [100, 2.5, -3]
 
+TNum = TypeVar('TNum', int, float)
 
-def main():
+
+def main() -> None:
     print(
         f"Answers:"
         f"\n\tAddition: {addition(x=addition_list)}"
@@ -17,19 +19,19 @@ def main():
     )
 
 
-def addition(x: List[SupportsFloat]) -> SupportsFloat:
+def addition(x: List[TNum]) -> TNum:
     pass
 
 
-def subtraction(x: List[SupportsFloat]) -> SupportsFloat:
+def subtraction(x: List[TNum]) -> TNum:
     pass
 
 
-def multiplication(x: List[SupportsFloat]) -> SupportsFloat:
+def multiplication(x: List[TNum]) -> TNum:
     pass
 
 
-def division(x: List[SupportsFloat]) -> Float:
+def division(x: List[TNum]) -> TNum:
     pass
 
 
--- learn_math/teacher_solutions/arithmetic.py
+++ learn_math/teacher_solutions/arithmetic.py
@@ -1,42 +1,44 @@
 """Methods for computing arithmetic calculations"""
-from typing import List, SupportsFloat
+from typing import List, TypeVar
 
 addition_list = [1.1, 2, 3, -4, 5]
 subtraction_list = [100, 99, -123.123, 0]
 multiplication_list = [2, 3, 5.5, -1]
 division_list = [100, 2.5, -3]
 
+TNum = TypeVar('TNum', int, float)
 
-def main():
+
+def main() -> None:
     print(
         f"Answers:"
         f"\n\tAddition: {addition(x=addition_list)}"
         f"\n\tSubtraction: {subtraction(x=subtraction_list)}"
-        f"\n\tMultiplcation: {multiplication(x=multiplication_list)}"
+        f"\n\tMultiplication: {multiplication(x=multiplication_list)}"
         f"\n\tDivision: {division(x=division_list)}"
     )
 
 
-def addition(x: List[SupportsFloat]) -> SupportsFloat:
+def addition(x: List[TNum]) -> TNum:
     return sum(x)
 
 
-def subtraction(x: List[SupportsFloat]) -> SupportsFloat:
-    value: SupportsFloat = x[0]
+def subtraction(x: List[TNum]) -> TNum:
+    value: TNum = x[0]
     for num in x[1:]:
         value -= num
     return value
 
 
-def multiplication(x: List[SupportsFloat]) -> SupportsFloat:
-    value: SupportsFloat = x[0]
+def multiplication(x: List[TNum]) -> TNum:
+    value: TNum = x[0]
     for num in x[1:]:
         value *= num
     return value
 
 
-def division(x: List[SupportsFloat]) -> Float:
-    value: SupportsFloat = x[0]
+def division(x: List[TNum]) -> TNum:
+    value: TNum = x[0]
     for num in x[1:]:
         value /= num
     return value
